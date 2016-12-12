5bccfe172c561074c124a574e928488576ae25c5
Kevin Terpstra <kkterpstra93@gmail.com>
2016-12-12 13:35:13 +0100

    Button delete all mechanics weg
    Dit omdat er nu per mechanic verwijderd kan worden

--- resources/views/mechanics/edit.blade.php
+++ resources/views/mechanics/edit.blade.php
@@ -1,18 +1,5 @@
 @extends('layouts.app')
 @section('content')
 
-<title>Contents are erased</title>
-<div class="container">
-    <h1>Contents are erased </h1>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-              Menu
-        </div>
-
-        <div class="panel-body">
-              Press here to return
-              <input type=button onclick="window.location='{{route("mechanics")}}'" value='terug'>
-<?php
-DB::table('mechanics')-> delete();
- ?>
+// hier moet een form komen
 @endsection
