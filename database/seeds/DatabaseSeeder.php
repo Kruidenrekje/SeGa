34b3be273ddb37d8eba23fe1112b93c5e56752cc
Kevin Terpstra <kkterpstra93@gmail.com>
2016-12-13 14:54:24 +0100

    Nieuwe lege database, met onze accounts erin
    op deze manier is er geen poep in de database en hoeven we niet elke
    keer een nieuw account te maken…

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      App\User::create([
+     'name' => 'Kevin Terpstra',
+     'email' => 'kkterpstra93@gmail.com',
+     'password' => bcrypt('kevin123'),
+     'remember_token' => str_random(10),
+      ]);
+
+      App\User::create([
+     'name' => 'Peter Westra',
+     'email' => 'west1300@student.nhl.nl',
+     'password' => bcrypt('peter123'),
+     'remember_token' => str_random(10),
+      ]);
+
+      App\User::create([
+     'name' => 'Paul Slotboom',
+     'email' => 'slotboom93@gmail.com',
+     'password' => bcrypt('paul123'),
+     'remember_token' => str_random(10),
+      ]);
+
+      App\User::create([
+     'name' => 'Michel van der Molen',
+     'email' => 'michelvdmolen1@gmail.com',
+     'password' => bcrypt('michel123'),
+     'remember_token' => str_random(10),
+      ]);
+
+      App\User::create([
+     'name' => 'Maikel Oppenhuizen',
+     'email' => 'maikeloppie1991@gmail.com',
+     'password' => bcrypt('maikel123'),
+     'remember_token' => str_random(10),
+      ]);
     }
 }
