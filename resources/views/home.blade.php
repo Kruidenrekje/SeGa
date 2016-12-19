376defb4093420154d12546a4487d81c2ee81aab
Michel <melodicmich@hotmail.com>
2016-12-19 11:05:44 +0100

    Edits in categories
    Edits in categories

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,7 @@
 			<hr>
 			<h3>Start browsing by selecting a category</h3>
 		</div>
-			<div class="row">
-                <div class="categories">
+			<div class="categories">
                 <div class="col-xs-6 col-md-4">
                     <div class="btn-group" role="group" aria-label="...">
                         <button type="button" class="btn btn-block">Goal</button>
@@ -35,8 +34,8 @@
                         <button type="button" class="btn btn-block">Puzzle</button>
                     </div>
                 </div>
-
-			</div>
+            </div>
         </div>
+    </div>
 </div>
 @endsection
